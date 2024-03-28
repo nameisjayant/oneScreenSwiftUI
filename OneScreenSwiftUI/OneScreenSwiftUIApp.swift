8e20f6363088fcb6f9dbb7d8cd5f92225b60e96f
nameisjayant <jayntkumar99@gmail.com>
2024-03-28 21:38:36 +0530

    init project

---
+++ OneScreenSwiftUI/OneScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneScreenSwiftUIApp.swift
+//  OneScreenSwiftUI
+//
+//  Created by jayant kumar on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct OneScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
